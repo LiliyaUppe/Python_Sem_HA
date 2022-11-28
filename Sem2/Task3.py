96109d4fdf3684748e77dcaa9b0a22dd2c295db7
Liliya Uppe <lilya30rus@mail.ru>
2022-11-28 15:54:00 +0400

    change Sem2/Task3.py

--- Sem2/Task3.py
+++ Sem2/Task3.py
@@ -8,10 +8,26 @@
 # print(count1)
 
 #variant2
-str1 = input()
-str2 = input()
+# str1 = input()
+# str2 = input()
+# count = 0
+# while str2 in str1:
+#     str1 = str1.replace(str2, '',1)
+#     count+=1
+# print(count)
+
+
+#variant3
+# str1 = input()
+# str2 = input()
+# print(int((len(str1) - len(str1.replace(str2, '')))/len(str2)))
+
+#variant4 (проверяем срез на соответствие)
+a = input()
+b = input()
 count = 0
-while str2 in str1:
-    str1 = str1.replace(str2, '', 1)
-    count += 1
+for i in range(len(a)):
+    if a[i:i+len(b)] == b:
+        count += 1
 print(count)
+print(a.count(b))
