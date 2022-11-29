e4917d3089f7230082632cdb8349e7c0ee2672c5
Liliya Uppe <lilya30rus@mail.ru>
2022-11-29 23:25:16 +0400

    add changes Task4.py

--- Sem1/Task4.py
+++ Sem1/Task4.py
@@ -14,16 +14,16 @@
 #варант3_вывод в строку как массив
 # n = int(input('N = '))
 # a = []
-# for i in range(-n,n+1):
-#     a.append(i) #.append добавляет элемент в конц списка
-#     print(f'{i}', end = ' ')
-# print()
+# # for i in range(-n,n+1):
+# #     a.append(i) #.append добавляет элемент в конц списка
+# #     print(f'{i}', end = ' ')
+# # print()
 # print(a)     
 
 #варант4_вывод в строку как массив
 # n = int(input('N = '))
 # a = []
-# b = ' '
+# b = ''
 # for i in range(-n,n+1):
 #     a.append(i) 
 #     b+= f' {i}'    
