2aeae1d1f0a4409429153edf35d9a6c69ad938e8
Liliya Uppe <lilya30rus@mail.ru>
2022-12-20 13:40:15 +0400

    add changes Sem3/HA_2.py

--- Sem3/HA_2.py
+++ Sem3/HA_2.py
@@ -11,20 +11,20 @@ list = [int(item) for item in a]
 print(list)
 multlist = []
 # if len(list)%2 == 0:
-for i in range (int(len(list)/2)+1):
+for i in range (int(len(list)+1)//2):  
     multlist.append(list[i]*list[(len(list)-i)-1])
 print(multlist)
 
 #teacher
 
-def pair_multi(nums: list[int] -> list[int]):
-    """return list of multiply pair"""
-    pairs = []
-    iterations = int((len(nums)+1)//2)
-    print(iterations)
-    for i in range(iterations):
-        pairs.append(nums[i]*nums[len(nums)-1-i])
-    return pairs
+# def pair_multi(nums: list[int] -> list[int]):
+#     """return list of multiply pair"""
+#     pairs = []
+#     iterations = int((len(nums)+1)//2)
+#     print(iterations)
+#     for i in range(iterations):
+#         pairs.append(nums[i]*nums[len(nums)-1-i])
+#     return pairs
 
-nums = [2, 3, 4 ,5 ,6]
-print(pair_multi(nums))
+# nums = [2, 3, 4 ,5 ,6]
+# print(pair_multi(nums))
